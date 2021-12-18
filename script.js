6189b8468e21a06c8144ba6613279a02872998f1
phvleite <phvleite@gmail.com>
2021-12-17 23:19:22 -0300

    salvando para continuar amanhã...

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const listaTarefa = [];
-
+const idElementoPaiLista = 'lista-tarefas';
 const tarefa = document.getElementById('texto-tarefa');
 const btInserirTarefa = document.getElementById('criar-tarefa');
 const inputTexto = document.getElementById('texto-tarefa');
 const btApagaTudo = document.getElementById('apaga-tudo');
+const btApagaFinalizados = document.getElementById('remover-finalizados');
+const classBasicaItemLista = 'item-tarefas';
+// const btSalvarTarefas = document.getElementById('salvar-tarefas');
+// const btMoverCima = document.getElementById('mover-cima');
+// const btMoverBaixo = document.getElementById('mover-baixo');
+// const btRemoverSelecionado = document.getElementById('remover-selecionados');
 
 function selecionarTarefa(evento) {
   const classeEvento = 'selecionado';
   const selecionado = document.getElementById(evento.target.id);
-  if (selecionado.className === `item-tarefas ${classeEvento}`) {
+  const slClNm = selecionado.className;
+  if (slClNm === `item-tarefas ${classeEvento}`) {
     selecionado.classList.remove(classeEvento);
-  // eslint-disable-next-line max-len
-  } else if (selecionado.className !== `item-tarefas ${classeEvento}` && selecionado.className !== 'item-tarefas completed') {
+  } else if (slClNm !== `item-tarefas ${classeEvento}` && slClNm !== 'item-tarefas completed') {
     const selecionadoAtual = document.querySelector(`.${classeEvento}`);
     if (selecionadoAtual) {
       selecionadoAtual.classList.remove(classeEvento);
     }
-    console.log(selecionado.classList);
     selecionado.classList.add(classeEvento);
-    console.log(selecionado.classList);
   }
 }
 
@@ -35,11 +39,29 @@ function riscarTarefa(evento) {
 }
 
 function acaoTarefas() {
-  const tarefas = document.getElementsByClassName('item-tarefas');
+  const tarefas = document.getElementsByClassName(classBasicaItemLista);
   for (let i = 0; i < tarefas.length; i += 1) {
     tarefas[i].addEventListener('click', selecionarTarefa);
     tarefas[i].addEventListener('dblclick', riscarTarefa);
   }
+}
+
+function recuperaLista() {
+  if (listaTarefa.length === 0) {
+    return;
+  }
+  const lista = document.getElementById(idElementoPaiLista);
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
+  const itemLista = document.createElement('li');
+  for (let i = 0; i < listaTarefa.length; i += 1) {
+    itemLista.innerText = listaTarefa[i];
+    itemLista.setAttribute('class', classBasicaItemLista);
+    itemLista.setAttribute('id', `${i}`);
+    lista.appendChild(itemLista);
+  }
+  acaoTarefas();
 }
 
 function insereTarefa() {
@@ -48,11 +70,11 @@ function insereTarefa() {
     return;
   }
   listaTarefa.push(tarefa.value);
-  const lista = document.getElementById('lista-tarefas');
+  const lista = document.getElementById(idElementoPaiLista);
   const itemLista = document.createElement('li');
   for (let i = 0; i < listaTarefa.length; i += 1) {
     itemLista.innerText = listaTarefa[i];
-    itemLista.setAttribute('class', 'item-tarefas');
+    itemLista.setAttribute('class', classBasicaItemLista);
     itemLista.setAttribute('id', `${i}`);
     lista.appendChild(itemLista);
   }
@@ -65,11 +87,33 @@ function apagaTudo() {
     alert('Lista vazia!');
     return;
   }
-  const lista = document.getElementById('lista-tarefas');
+  const lista = document.getElementById(idElementoPaiLista);
   while (lista.firstChild) {
     lista.removeChild(lista.firstChild);
   }
   listaTarefa.splice(0, listaTarefa.length);
+}
+
+function apagaFinalizados() {
+  if (listaTarefa.length === 0) {
+    alert('Lista vazia!');
+    return;
+  }
+  let lista = document.querySelectorAll('.completed');
+  if (lista.length === 0) {
+    alert('Não existem tarefas finalizadas!');
+    return;
+  }
+  for (let i = 0; i < lista.length; i += 1) {
+    lista[i].parentElement.remove(lista[i]);
+  }
+  listaTarefa.splice(0, listaTarefa.length);
+  lista = document.getElementById(idElementoPaiLista);
+  console.log(lista);
+/*   for (let i = 0; i < lista.length; i += 1) {
+    listaTarefa.push(lista[i].innerText);
+  } */
+  // recuperaLista();
 }
 
 function verificaEnter(evento) {
@@ -82,3 +126,4 @@ function verificaEnter(evento) {
 btInserirTarefa.addEventListener('click', insereTarefa);
 inputTexto.addEventListener('keyup', verificaEnter);
 btApagaTudo.addEventListener('click', apagaTudo);
+btApagaFinalizados.addEventListener('click', apagaFinalizados);
